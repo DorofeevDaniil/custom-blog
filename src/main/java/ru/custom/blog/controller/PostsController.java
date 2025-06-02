c5eeb08c56aa26ada3def609072b76c90ef0f592
DorofeevDaniil <dorofeefdi@gmail.com>
2025-06-02 18:34:08 +0300

    added edit post endpoint

--- src/main/java/ru/custom/blog/controller/PostsController.java
+++ src/main/java/ru/custom/blog/controller/PostsController.java
@@ -21,6 +21,8 @@ public class PostsController {
 
     private final PostService postService;
 
+    private static final String REDIRECT_POSTS = "redirect:/posts/";
+
     @Autowired
     public PostsController(PostService postService) {
         this.postService = postService;
@@ -59,7 +61,7 @@ public class PostsController {
         PostModel post = new PostModel(title, 0, Arrays.stream(tags.split(" ")).toList(), text);
         Long postId = postService.savePost(post, image, request.getServletContext().getRealPath(""));
 
-        return "redirect:/posts/" + postId;
+        return REDIRECT_POSTS + postId;
     }
 
     @PostMapping("/posts/{id}")
@@ -74,7 +76,7 @@ public class PostsController {
         PostModel post = new PostModel(id, title, Arrays.stream(tags.split(" ")).toList(), text);
         postService.editPost(post, image, request.getServletContext().getRealPath(""));
 
-        return "redirect:/posts/" + id;
+        return REDIRECT_POSTS + id;
     }
 
     @GetMapping("/posts/{id}")
@@ -94,6 +96,6 @@ public class PostsController {
 
         postService.updateLikesCount(id, like);
 
-        return "redirect:/posts/" + id;
+        return REDIRECT_POSTS + id;
     }
 }
