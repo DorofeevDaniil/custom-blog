2bf860e19ade0d797404d1bcfd487560fb42cb10
DorofeevDaniil <dorofeefdi@gmail.com>
2025-06-02 15:12:59 +0300

    added resources and configurations with partitial template fixes

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,22 @@
+create table if not exists posts(
+    id bigserial primary key,
+    title varchar(256) not null,
+    text varchar(1000000000) not null,
+    image_path varchar(256) not null,
+    likes_count integer not null,
+    tags varchar(500) not null
+);
+
+create table if not exists comments(
+    id bigserial primary key,
+    post_id bigint not null,
+    text varchar(1000000000) not null,
+    foreign key (post_id) references posts(id)
+);
+
+create table if not exists tags(
+    id bigserial primary key,
+    post_id bigint not null,
+    tag varchar(20) not null,
+    foreign key (post_id) references posts(id)
+);
