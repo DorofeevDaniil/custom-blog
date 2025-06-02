e5fd3cdead79c253a868fbffdbf6fd6edcb6b7be
DorofeevDaniil <dorofeefdi@gmail.com>
2025-06-02 17:01:12 +0300

    added post repository method

--- src/main/java/ru/custom/blog/repository/PostRepository.java
+++ src/main/java/ru/custom/blog/repository/PostRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface PostRepository {
     List<PostModel> findPage(Integer limit, Integer offset);
+    PostModel findPostById(Long id);
     Long save(PostModel post);
     void deleteById(Long id);
     Long getTotalCount();
